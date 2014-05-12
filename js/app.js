7fe41cd7f49b3239f756df63bf2df17be37b2e90
Thomas Abraham <thomas@nerdowboy.com>
2014-05-12 22:56:33 +0530

    Projects displayed from file.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 App = Ember.Application.create();
 
 App.Router.map(function(){
-	this.resource('about');
+	this.resource('home',{path:'/'});
+});
+
+App.projects = Ember.Object.create({
+	myprojects:[]
+});
+
+$(function(){
+	$.get('projects',function(data){
+		App.projects.myprojects.setObjects(eval(data));
+	});
 });
